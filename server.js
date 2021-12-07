2262d872a70d523bfe938d273830a99dabfedaec
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-06 22:51:33 -0800

    null

--- server.js
+++ server.js
@@ -19,16 +19,18 @@ app.use(express.json());
 
 
 
+
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 app.post('/api/notes', (req, res) => {
