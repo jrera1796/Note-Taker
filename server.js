d0b28d2761bbe2db407133995d9876a2a113f2a3
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-06 22:24:18 -0800

    Debugging heroku routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const crypto = require('crypto');
+const id = crypto.randomBytes(10).toString('hex');
 const PORT = process.env.PORT || 3001;
+
+
+// function filterItems();
 
 //'middleware'
 app.use(express.static('public'));
@@ -22,19 +28,75 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'));
+  res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 app.post('/api/notes', (req, res) => {
-  if (req.body){
-console.log(req.body);
+  const { title, text } = req.body;
+  if (title && text) {
+    //reading content
+    fs.readFile(`./db/db.json`, 'utf-8', (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        //parsing contents
+        const dbNotes = JSON.parse(data);
+        //appending POST body
+        dbNotes.push({ title, text, id: new crypto.randomBytes(10).toString('hex') });
+        //rewriting with new changes
+        fs.writeFile(`./db/db.json`, JSON.stringify(dbNotes), (err) => err ?
+          console.error(err) : console.log(`${title} is written to JSON file`));
+      }
+    }) 
+    res.json({Status: 'Success', body : {title, text, id}})
+  } else {
+    res.json('Could not add note');
   }
 });
 
-app.get('/api/notes', (req, res) => {
-  
+
+app.delete('/api/notes/:id', (req, res) => {
+  //reading file
+  fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+      if (err) { console.log(err); }
+      //parsing contents
+      const dbNotes = JSON.parse(data);
+      for (let i = 0; i < dbNotes.length; i++) {
+          if (dbNotes[i].id === req.params.id) { dbNotes.splice(i, 1); }
+      }
+      fs.writeFile(`./db/db.json`, JSON.stringify(dbNotes), (err) => err ?
+          console.error(err) : console.log(`Note deleted ID: ${req.params.id}`)
+      );
+      res.send(dbNotes);
+  })
+});
+
+
+//-----TESTING-AREA---REMOVE-OUT-ONCE-SUCCESSFUL-----------
+
+// app.delete('/api/notes/:id', (req, res) => {
+//   //reading file
+//   fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+//       if (err) { console.log(err); }
+//       //parsing contents
+//       const dbNotes = JSON.parse(data);
+//       for (let i = 0; i < dbNotes.length; i++) {
+//           if (dbNotes[i].id === req.params.id) { dbNotes.splice(i, 1); }
+//       }
+//       fs.writeFile(`./db/db.json`, JSON.stringify(dbNotes), (err) => err ?
+//           console.error(err) : console.log(`Note deleted ID: ${req.params.id}`)
+//       );
+//       res.send(dbNotes);
+//   })
+// });
+
+
+//-------------------------------------------------------
+app.get('*', (req, res) => {
+  res.sendFile(__dirname, './public/index.html');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
