6ae309f5114249c5c9f42977f6e6ee0bba14ec3b
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-07 22:16:30 -0800

    cleaning code and finalizing project

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const crypto = require('crypto');
 const id = crypto.randomBytes(10).toString('hex');
 const PORT = process.env.PORT || 3001;
 
-
-// function filterItems();
-
 //'middleware'
 app.use(express.static('public'));
 // parse incoming string or array data
@@ -17,17 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-
-
-
-
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './db/db.json'));
 });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'));
-// });
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -73,27 +62,6 @@ app.delete('/api/notes/:id', (req, res) => {
   })
 });
 
-
-//-----TESTING-AREA---REMOVE-OUT-ONCE-SUCCESSFUL-----------
-
-// app.delete('/api/notes/:id', (req, res) => {
-//   //reading file
-//   fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
-//       if (err) { console.log(err); }
-//       //parsing contents
-//       const dbNotes = JSON.parse(data);
-//       for (let i = 0; i < dbNotes.length; i++) {
-//           if (dbNotes[i].id === req.params.id) { dbNotes.splice(i, 1); }
-//       }
-//       fs.writeFile(`./db/db.json`, JSON.stringify(dbNotes), (err) => err ?
-//           console.error(err) : console.log(`Note deleted ID: ${req.params.id}`)
-//       );
-//       res.send(dbNotes);
-//   })
-// });
-
-
-//-------------------------------------------------------
 app.get('*', (req, res) => {
   res.sendFile(__dirname, './public/index.html');
 });
